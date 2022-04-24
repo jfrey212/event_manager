314047ca5592b7c8c9d956f42d54b3a3659f8fa8
jfrey212 <eojyerf@gmail.com>
2022-04-24 18:47:47 -0500

    Used rjust and [] to fix short and long zip codes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,9 +8,11 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = row[:zipcode]
-  # if the zip code is exactly five digits, assume that it is ok
-  # if the zip code is more than five digits, truncate it to the first five digits
-  # if the zip code is less than five digits, add zeros to the front until it becomes five digits
+  if zipcode < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode > 5
+    zipcode = zipcode[0..4]
+  end
 
   puts "#{name} #{zipcode}"
 end
