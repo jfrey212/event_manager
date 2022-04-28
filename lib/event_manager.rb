bd4a6f5e8894252ec120c6d7c4ab39c967fd7941
jfrey212 <eojyerf@gmail.com>
2022-04-28 00:58:16 -0500

    Added comments to clean_zip method and made Rubocop edits

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,20 +1,26 @@
 require 'csv'
 
+# to_s deals with empty values (nil) -> nil.to_s = ""
+# to_s does not affect the non-nil values
+# rjust(5, '0') adds leading zeros to short zip codes
+# does nothing for zip codes >= 5 digits
+# slice [0..4] take the first 5 digits from a long zip code
+# does nothing to a code with exactly 5 digits
 def clean_zipcode(zipcode)
-    zipcode.to_s.rjust(5, '0')[0..4]
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 
-puts "Event Manager Initialized"
+puts 'Event Manager Initialized'
 
 contents = CSV.open(
   '../event_attendees.csv',
   headers: true,
-  header_converters: :symbol)
+  header_converters: :symbol
+)
 
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-
 
   puts "#{name} #{zipcode}"
 end
