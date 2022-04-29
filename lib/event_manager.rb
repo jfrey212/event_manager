7615511ddd2982edcf27c6379de53af9b6471f4a
jfrey212 <eojyerf@gmail.com>
2022-04-29 17:49:40 -0500

    Added erb functionality to form letter and ruby code

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 # to_s deals with empty values (nil) -> nil.to_s = ""
 # to_s does not affect the non-nil values
@@ -20,14 +21,22 @@ def legislators_by_zipcode(zip)
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislators_string = legislator_names.join(", ")
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts 'Event Manager Initialized'
 
 contents = CSV.open(
@@ -36,14 +45,18 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
-end
+  form_letter = erb_template.result(binding)
 
-template_letter = File.read('form_letter.html')
+  save_thank_you_letter(id, form_letter)
+end
